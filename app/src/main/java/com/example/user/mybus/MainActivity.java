d624f43110199cd62eaba22c699abd6cf32c2cae
HyperionNKJ <kjneo1996@gmail.com>
2019-06-30 20:53:18 +0800

    Refactor MainActivity Checks code

--- app/src/main/java/com/example/user/mybus/MainActivity.java
+++ app/src/main/java/com/example/user/mybus/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.user.mybus;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -19,15 +18,17 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import static util.Config.GPS_CHECK_ENABLED;
+import static util.Config.LOCATION_PERMISSION_CHECK_ENABLED;
+import static util.Config.PLAY_SERVICE_CHECK_ENABLED;
 import static util.Constants.ERROR_DIALOG_REQUEST;
 import static util.Constants.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
-import static util.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private boolean mLocationPermissionGranted = false;
+    private GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,120 +41,120 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         /*
-          Three checks before app starts.
-          1) Google Play Services is installed
+          Three checks before app starts. Disable in util/Config
+          1) Google Play Services is installed and working
           2) GPS is enabled
           3) Location Permission is granted
          */
-        if (isServicesOK() && isMapsEnabled()) {
-            if (mLocationPermissionGranted) {
-                startMyBus();
-            } else {
-                getLocationPermission();
-            }
+        boolean[] checkResults = new boolean[3];
+        while (!allCheckPasses(checkResults)) {
+            requestForAction(checkResults);
+        }
+        startMyBus();
+    }
+
+    private boolean allCheckPasses(boolean[] checkResults) {
+        boolean playServicePasses = !PLAY_SERVICE_CHECK_ENABLED || isPlayServicesOK();
+        checkResults[0] = playServicePasses;
+
+        boolean GPSPasses = !GPS_CHECK_ENABLED || isGPSEnabled();
+        checkResults[1] = GPSPasses;
+
+        boolean locationPermissionPasses = !LOCATION_PERMISSION_CHECK_ENABLED || isLocationPermitted();
+        checkResults[2] = locationPermissionPasses;
+
+        return playServicePasses && GPSPasses && locationPermissionPasses;
+    }
+
+    private void requestForAction(boolean[] checkResults) {
+        if (!checkResults[0]) {
+            requestForPlayService();
+        }
+        if (!checkResults[1]) {
+            requestForGPS();
+        }
+        if (!checkResults[2]) {
+            requestForLocation();
         }
     }
 
     // Checks for Google Play Services
-    public boolean isServicesOK() {
-        Log.d(TAG, "isServicesOK: Checking google services ...");
-        GoogleApiAvailability instance = GoogleApiAvailability.getInstance();
+    private boolean isPlayServicesOK() {
+        Log.d(TAG, "isPlayServicesOK: Checking google services ...");
+        return googleApiAvailability.isGooglePlayServicesAvailable(MainActivity.this)
+                == ConnectionResult.SUCCESS;
+    }
 
-        int available = instance.isGooglePlayServicesAvailable(MainActivity.this);
-        switch (available) {
-            case ConnectionResult.SUCCESS:
-                Log.d(TAG, "isServicesOK: Google Play Service is up-to-date and can connect");
-                return true;
+    // Request to update / install Google Play Services
+    private void requestForPlayService() {
+        Log.d(TAG, "requestForPlayService: Google Play Services failed. Checking why ...");
+        int availability = googleApiAvailability.isGooglePlayServicesAvailable(MainActivity.this);
+        switch (availability) {
             case ConnectionResult.SERVICE_MISSING: case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED: case ConnectionResult.SERVICE_DISABLED:
-                Log.d(TAG, "isServicesOK: Google Play Service outdated. Need to update");
-                Dialog dialog = instance.getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+                Log.d(TAG, "requestForPlayService: Google Play Service outdated. Need to update");
+                Dialog dialog = googleApiAvailability.getErrorDialog(MainActivity.this, availability, ERROR_DIALOG_REQUEST);
                 dialog.show();
                 break;
             default:
-                Log.d(TAG, "isServicesOK: Google Play Services failed");
+                Log.d(TAG, "requestForPlayService: Google Play Services failed for no reason.");
                 Toast.makeText(this, "Google Play Services failed", Toast.LENGTH_SHORT).show();
                 break;
         }
-        return false;
     }
 
     // Check if GPS enabled
-    public boolean isMapsEnabled(){
-        Log.d(TAG, "isMapsEnabled: Checking GPS availability ...");
+    private boolean isGPSEnabled(){
+        Log.d(TAG, "isGPSEnabled: Checking GPS availability ...");
         final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
-
-        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
-            Log.d(TAG, "isMapsEnabled: GPS is disabled");
-            buildAlertMessageNoGps(); // if GPS disabled, prompt user to enable it
-            return false;
-        }
-        Log.d(TAG, "isMapsEnabled: GPS is enabled");
-        return true;
+        return manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 
+    // Request to enable GPS
+    private void requestForGPS() {
+        Log.d(TAG, "requestForGPS: GPS is disabled. Prompting user to enable ...");
+        buildAlertMessageNoGps(); // if GPS disabled, prompt user to enable it
+    }
+
+    // Request to enable GPS - build dialog
     private void buildAlertMessageNoGps() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
                 .setCancelable(false)
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Please enable GPS for full functionality of MyBus.", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                })
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     // opens up setting screen to enable GPS
                     public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
                         Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        // SettingActivity will send result back here. Callback "onActivityResult (below)" automatically called.
-                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS); // 2nd arg is requestCode
+                        startActivity(enableGpsIntent); // 2nd arg is requestCode
                     }
                 });
-        final AlertDialog alert = builder.create();
-        Log.d(TAG, "buildAlertMessageNoGps: Request dialog to enable GPS ...");
+        AlertDialog alert = builder.create();
         alert.show();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ENABLE_GPS: {
-                // Check if location permission granted
-                if(mLocationPermissionGranted){
-                    Log.d(TAG, "onActivityResult: Location permission granted. Starting app.");
-                    startMyBus();
-                }
-                else{
-                    Log.d(TAG, "onActivityResult: Location permission not granted.");
-                    getLocationPermission(); // if not granted, request for it
-                }
-            }
-        }
+    // Check if location permission is granted
+    private boolean isLocationPermitted() {
+        Log.d(TAG, "isLocationPermitted: Checking location permission ...");
+        return ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void getLocationPermission() {
-        // Request location permission, so that we can get the location of the device.
-
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-            startMyBus();
-        } else {
-            Log.d(TAG, "getLocationPermission: Getting permission ...");
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-            // The result of the permission request is handled by a callback, onRequestPermissionsResult (below).
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        mLocationPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION:
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
-                }
-        }
+    // Request for location permission
+    private void requestForLocation() {
+        Log.d(TAG, "requestForLocation: Location permission not granted. Requesting for permission ...");
+        // The result of the permission request is handled by a callback, onRequestPermissionsResult (below).
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
     }
 
     private void startMyBus() {
+        Log.d(TAG, "startMyBus: Starting MyBus ...");
         Intent intent = new Intent(MainActivity.this, MapsActivity.class);
         startActivity(intent);
     }
---
+++ app/src/main/java/util/Config.java
@@ -0,0 +1,8 @@
+package util;
+
+public class Config {
+    public static final boolean PLAY_SERVICE_CHECK_ENABLED = true;
+    public static final boolean GPS_CHECK_ENABLED = true;
+    public static final boolean LOCATION_PERMISSION_CHECK_ENABLED = true;
+
+}
