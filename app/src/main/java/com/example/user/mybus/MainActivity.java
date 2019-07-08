a0874e0db725effc3914411123c812d94194ff1e
HyperionNKJ <kjneo1996@gmail.com>
2019-07-08 20:51:45 +0800

    Google Play Services, GPS, Location Permission Checks - Refactored

--- app/src/main/java/com/example/user/mybus/MainActivity.java
+++ app/src/main/java/com/example/user/mybus/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import static util.Config.PRECHECK_ENABLED;
 import static util.Constants.ERROR_DIALOG_REQUEST;
 import static util.Constants.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
 import static util.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private boolean mLocationPermissionGranted = false;
+    private boolean firstPermissionCallback = true; // for logD usage
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,121 +40,148 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
-        /*
-          Three checks before app starts.
-          1) Google Play Services is installed
-          2) GPS is enabled
-          3) Location Permission is granted
-         */
-        if (isServicesOK() && isMapsEnabled()) {
-            if (mLocationPermissionGranted) {
-                startMyBus();
-            } else {
-                getLocationPermission();
+        if (PRECHECK_ENABLED) {
+            /*
+              Three checks before app starts.
+              1) Google Play Services is installed
+              2) GPS is enabled
+              3) Location Permission is granted
+            */
+            if (isPlayServicesOK() && isMapsEnabled()) {
+                if (mLocationPermissionGranted) {
+                    Log.d(TAG, "onResume: Location permission granted. Starting app...");
+                    startMyBus();
+                } else {
+                    checkLocationPermission();
+                }
             }
+            /*
+              if isPlayServicesOK / isMapsEnabled is false, no further execution will occur on main thread,
+              allowing UI thread (corresponding dialogs) to "block" and wait for user's input.
+            */
+        } else {
+            Log.d(TAG, "onResume: Precheck DISABLED. Starting app straight...");
+            startMyBus(); // if config disables precheck, start MyBus straight away.
         }
     }
 
-    // Checks for Google Play Services
-    public boolean isServicesOK() {
-        Log.d(TAG, "isServicesOK: Checking google services ...");
+    // Check if Google Play Services is installed
+    public boolean isPlayServicesOK() {
+        Log.d(TAG, "isPlayServicesOK: Checking google services...");
         GoogleApiAvailability instance = GoogleApiAvailability.getInstance();
-
         int available = instance.isGooglePlayServicesAvailable(MainActivity.this);
+
         switch (available) {
             case ConnectionResult.SUCCESS:
-                Log.d(TAG, "isServicesOK: Google Play Service is up-to-date and can connect");
+                Log.d(TAG, "isPlayServicesOK: Google Play Service is up-to-date and can connect");
                 return true;
-            case ConnectionResult.SERVICE_MISSING: case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED: case ConnectionResult.SERVICE_DISABLED:
-                Log.d(TAG, "isServicesOK: Google Play Service outdated. Need to update");
+            case ConnectionResult.SERVICE_MISSING:
+            case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+            case ConnectionResult.SERVICE_DISABLED:
+                Log.d(TAG, "isPlayServicesOK: Google Play Service outdated or not installed. Prompting dialog request...");
                 Dialog dialog = instance.getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
                 dialog.show();
                 break;
             default:
-                Log.d(TAG, "isServicesOK: Google Play Services failed");
+                Log.d(TAG, "isPlayServicesOK: Google Play Services failed");
                 Toast.makeText(this, "Google Play Services failed", Toast.LENGTH_SHORT).show();
                 break;
         }
-        return false;
+        return false; // no more code in main thread, allowing UI thread carrying dialog to block
     }
 
-    // Check if GPS enabled
+    // Check if GPS is enabled
     public boolean isMapsEnabled(){
         Log.d(TAG, "isMapsEnabled: Checking GPS availability ...");
-        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+        if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d(TAG, "isMapsEnabled: GPS is enabled");
+            return true;
+        } else {
             Log.d(TAG, "isMapsEnabled: GPS is disabled");
             buildAlertMessageNoGps(); // if GPS disabled, prompt user to enable it
-            return false;
+            return false; // no more code in main thread, allowing UI thread carrying dialog to block
         }
-        Log.d(TAG, "isMapsEnabled: GPS is enabled");
-        return true;
     }
 
+    // If GPS is disabled, prompt dialog to enable GPS in settings
     private void buildAlertMessageNoGps() {
+        Log.d(TAG, "buildAlertMessageNoGps: Prompting dialog request to enable GPS...");
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
                 .setCancelable(false)
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    // opens up setting screen to enable GPS
                     public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
                         Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        // SettingActivity will send result back here. Callback "onActivityResult (below)" automatically called.
                         startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS); // 2nd arg is requestCode
+                        // Direct user to settings to enable GPS. Callback "onActivityResult (below)" automatically called upon returning.
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        buildAlertMessageNoGps();
                     }
                 });
         final AlertDialog alert = builder.create();
-        Log.d(TAG, "buildAlertMessageNoGps: Request dialog to enable GPS ...");
         alert.show();
     }
 
+    // Callback when user returns from GPS setting page
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ENABLE_GPS: {
-                // Check if location permission granted
-                if(mLocationPermissionGranted){
-                    Log.d(TAG, "onActivityResult: Location permission granted. Starting app.");
-                    startMyBus();
-                }
-                else{
-                    Log.d(TAG, "onActivityResult: Location permission not granted.");
-                    getLocationPermission(); // if not granted, request for it
-                }
+        Log.d(TAG, "onActivityResult: User has returned from GPS setting page");
+
+        // Check if location permission granted
+        if (requestCode == PERMISSIONS_REQUEST_ENABLE_GPS) {
+            if (mLocationPermissionGranted) {
+                Log.d(TAG, "onActivityResult: Location permission granted. Starting app...");
+                startMyBus();
+            } else {
+                checkLocationPermission();
             }
         }
     }
 
-    private void getLocationPermission() {
-        // Request location permission, so that we can get the location of the device.
+    // Checks if location permission is granted. If not granted, request for it via permission dialog.
+    private void checkLocationPermission() {
+        Log.d(TAG, "getLocationPermission: Checking if location permission is granted...");
 
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "getLocationPermission: Location permission granted. Starting app...");
             mLocationPermissionGranted = true;
             startMyBus();
         } else {
-            Log.d(TAG, "getLocationPermission: Getting permission ...");
+            Log.d(TAG, "getLocationPermission: Location permission not granted. Prompting dialog request to obtain location permission...");
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-            // The result of the permission request is handled by a callback, onRequestPermissionsResult (below).
+            // Callback "onRequestPermissionsResult (below)" automatically called upon user response.
         }
     }
 
+    // Callback when user responds to permission dialog
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (firstPermissionCallback) {
+            // First callback is false alarm, don't show log
+            firstPermissionCallback = false;
+        } else {
+            Log.d(TAG, "onRequestPermissionsResult: User has responded to permission dialog");
+        }
+
         mLocationPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION:
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
-                }
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+            }
         }
     }
 
+    // Start MyBus
     private void startMyBus() {
         Intent intent = new Intent(MainActivity.this, MapsActivity.class);
         startActivity(intent);
---
+++ app/src/main/java/util/Config.java
@@ -0,0 +1,5 @@
+package util;
+
+public class Config {
+    public static final boolean PRECHECK_ENABLED = false;
+}
