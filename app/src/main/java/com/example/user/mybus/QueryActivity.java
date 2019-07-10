34fc5712891b54c333eb2978c36e94b15a95cf96
HyperionNKJ <kjneo1996@gmail.com>
2019-07-10 21:35:53 +0800

    Add map fragment and toggle button

--- app/src/main/java/com/example/user/mybus/QueryActivity.java
+++ app/src/main/java/com/example/user/mybus/QueryActivity.java
@@ -4,6 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -11,25 +17,31 @@ import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import static util.Config.DEFAULT_MAP_TYPE;
 import static util.Config.PLACE_FIELDS;
 import static util.Constants.ARRIVAL;
 import static util.Constants.DEPARTURE;
 
-public class QueryActivity extends AppCompatActivity {
+public class QueryActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "QueryActivity";
 
+    private GoogleMap googleMap;
     private PlacesClient placesClient;
     private Place departurePlace;
     private Place arrivalPlace;
     private Spinner departureRadius;
     private Spinner arrivalRadius;
+    private Button mapTypeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +55,51 @@ public class QueryActivity extends AppCompatActivity {
         Places.initialize(getApplicationContext(), getString(R.string.google_maps_key));
         placesClient = Places.createClient(this);
 
+        setupGoogleMap();
         setupAutocompleteSearch(DEPARTURE);
         setupRadiusSpinner(DEPARTURE);
         setupAutocompleteSearch(ARRIVAL);
         setupRadiusSpinner(ARRIVAL);
+    }
+
+    private void setupGoogleMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap gMap) {
+        googleMap = gMap;
+        googleMap.setMapType(DEFAULT_MAP_TYPE);
+        setupToggleButton();
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void setupToggleButton() {
+        mapTypeButton = findViewById(R.id.b_map_type_toggle);
+        updateButtonIcon();
+
+        mapTypeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (googleMap.getMapType() == GoogleMap.MAP_TYPE_HYBRID) {
+                    googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                } else {
+                    googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                }
+                updateButtonIcon();
+            }
+        });
+    }
+
+    private void updateButtonIcon() {
+        int appropriateIconID = (googleMap.getMapType() == GoogleMap.MAP_TYPE_HYBRID) ? R.drawable.b_toggle_default : R.drawable.b_toggle_satellite;
+        mapTypeButton.setBackground(getResources().getDrawable(appropriateIconID));
     }
 
     private void setupAutocompleteSearch(int type) {
--- app/src/main/java/util/Config.java
+++ app/src/main/java/util/Config.java
@@ -2,10 +2,13 @@ package util;
 
 import java.util.Arrays;
 import java.util.List;
+
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.libraries.places.api.model.Place;
 
 
 public class Config {
     public static final boolean PRECHECK_ENABLED = false;
     public static final List<Place.Field> PLACE_FIELDS = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG);
+    public static final int DEFAULT_MAP_TYPE = GoogleMap.MAP_TYPE_HYBRID;
 }
